07186ef18746ab2945393f9c3612e30011191f85
Grzegorz Szamburski <szamburski.grzegorz@wp.pl>
2013-11-18 19:05:40 +0100

    Start

---
+++ aplikacja/module/Application/src/Uzytkownik/Controller/RejestracjaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Aplikacja\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class RejestracjaController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return array();
+    }
+
+    public function fooAction()
+    {
+        // This shows the :controller and :action parameters in default route
+        // are working when you browse to /module-specific-root/skeleton/foo
+        return array();
+    }
+}
